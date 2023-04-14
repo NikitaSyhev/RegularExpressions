37f247112677e400e9a23af772d76223d7778bc7
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2023-04-14 21:26:13 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using RegularExpressions;
+
+
+
+namespace RegularExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "23 * 45 =";
+            var regexpLeft = new Regex(@"\d+\s*(\+|\-|\*|\/)");
+            var regexpRight = new Regex(@"(\+|\-|\*|\/)\s*\d+");
+            var regNumber = new Regex(@"\d+");
+            var regexpOperation  = new Regex(@"\d+\s*(\+|\-|\*|\/)");
+            MatchCollection matchesLeft = regexpLeft.Matches(input);
+            MatchCollection matchesRight = regexpRight.Matches(input);
+            foreach (Match match in matchesLeft)
+            {
+                Console.WriteLine("Левый операнд и орперация: " + match.ToString());
+                MatchCollection numberLeft = regNumber.Matches(match.ToString());
+                Console.WriteLine("Правый операнд: " + numberLeft[0]);
+
+            }
+            foreach (Match match in matchesRight)
+            {
+                Console.WriteLine("Левый операнд и орперация: " + match.ToString());
+                MatchCollection numberRight = regNumber.Matches(match.ToString());
+                Console.WriteLine("Правый операнд: " + numberRight[0]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("RegulatExpressions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegulatExpressions")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("210718d9-c8e6-4a0b-9abd-d62c1f1523e8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
